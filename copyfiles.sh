b1a5b027b606a70a6e52dea76e2db53ea90626aa
RadicalLoop <hello@radicalloop.com>
2016-07-28 14:22:20 +0530

    Delete copyfiles.sh

--- copyfiles.sh
+++
@@ -1,8 +0,0 @@
-SRC_DIR="/source/directory/path/"
-DEST_DIR="/destination/directory/path/"
-for file in $(<filelist.txt); 
-do
-	SCRIPTPATH=$(dirname "$file")
-	DIRPERM=`stat -c "%a" $SRC_DIR$SCRIPTPATH`	
-	mkdir -p --mode=$DIRPERM $DEST_DIR$SCRIPTPATH && cp $SRC_DIR$file $DEST_DIR$file
-done
