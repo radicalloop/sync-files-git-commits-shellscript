0aaaa2746d73f71925038b3d7803da2be847e1ac
RadicalLoop <hello@radicalloop.com>
2016-08-24 17:10:00 +0530

    Update syncfiles.sh
    avoid duplicate filename listing

--- syncfiles.sh
+++ syncfiles.sh
@@ -10,44 +10,47 @@ read commits
 
 if [[ $commits > 0 ]]; then
 
-	hashlist=`git log --format="%H" -n $commits`
+    hashlist=`git log --format="%H" -n $commits`
 
-	echo ----------Files------------
-	
-	for hashnum in $hashlist;
-	do
-		filelist=`git log -m -1 --name-only --pretty="format:" $hashnum`
-		for filename in $filelist;
-		do
-			echo $filename >> filelist-$dt.txt
-		done		
-	done
-	cat filelist-$dt.txt
-	echo ---------------------------
-	echo 'Proceed copying/syncing above files? [y/n]'	
-	read copyans
-	if [[ $copyans == 'y' || $copyans == 'Y' ]]; then
-		for file in $(<filelist-$dt.txt); 
-		do
-			SCRIPTPATH=$(dirname "$file")
-			
-			if [ -f "$file" ] 
-			then
-				{
-					DIRPERM=`stat -c "%a" $SRC_DIR$SCRIPTPATH`	
-					mkdir -p --mode=$DIRPERM $DEST_DIR$SCRIPTPATH && cp $SRC_DIR$file $DEST_DIR$file 
-				}
-			elif [[ -f "$DEST_DIR$file" ]]; 
-			then
-				rm -r $DEST_DIR$file 
-			else
-				echo $DEST_DIR$file ' - file does not exist.'
-			fi
+    echo ----------Files------------
 
-		done
-		echo "Done syncing."	
-	else
-		echo "Terminated."
-	fi
+    for hashnum in $hashlist;
+    do
+        filelist=`git log -m -1 --name-only --pretty="format:" $hashnum`
+        for filename in $filelist;
+        do
+            if ! grep -Fxq "$filename" filelist-$dt.txt
+            then
+               echo $filename >> filelist-$dt.txt
+            fi
+        done
+    done
+    cat filelist-$dt.txt
+    echo ---------------------------
+    echo 'Proceed copying/syncing above files? [y/n]'
+    read copyans
+    if [[ $copyans == 'y' || $copyans == 'Y' ]]; then
+        for file in $(<filelist-$dt.txt);
+        do
+            SCRIPTPATH=$(dirname "$file")
+
+            if [ -f "$file" ]
+            then
+                {
+                    DIRPERM=`stat -c "%a" $SRC_DIR$SCRIPTPATH`
+                    mkdir -p --mode=$DIRPERM $DEST_DIR$SCRIPTPATH && cp $SRC_DIR$file $DEST_DIR$file
+                }
+            elif [[ -f "$DEST_DIR$file" ]];
+            then
+                rm -r $DEST_DIR$file
+            else
+                echo $DEST_DIR$file ' - file does not exist.'
+            fi
+
+        done
+        echo "Done syncing."
+    else
+        echo "Terminated."
+    fi
 fi
 rm -f filelist-$dt.txt
